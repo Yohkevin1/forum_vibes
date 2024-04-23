9681d3da9ce62dbe2347fd14ad252be14677ce61
Yohkevin1 <yohkevinwu@gmail.com>
2024-04-23 18:22:32 +0700

    make test error

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -16,7 +16,7 @@ export default function RegisterPage() {
     return (
         <div className="container_register_login">
             <aside className="side">
-                <h1 className="title">ForumVibes</h1>
+                <h1 className="title">ForumVibes APP</h1>
                 <p>
                     Sudah punya akun?<Link to="/login">Login</Link>
                 </p>
