3402e343dc1726c6160944106576f02e7551da41
Yohkevin1 <yohkevinwu@gmail.com>
2024-04-24 16:41:01 +0700

    asal

--- src/states/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './rootReducer';
-import { thunk } from './middleware';
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
